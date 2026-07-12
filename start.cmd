f6fb96517fdae7a73866067eb1e426b6ad6c101b
ocvcoin <90601304+ocvcoin@users.noreply.github.com>
2026-07-12 18:30:01 +0000

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,61 +1,31 @@
 @ECHO OFF
+SETLOCAL DisableDelayedExpansion
+
 IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    REM Running under 64-bit CMD.EXE on 64-bit Windows...
-    SET builtin_python_path="python-3.6.0-embed-amd64"
+    SET "builtin_python_path=python-3.6.0-embed-amd64"
 ) ELSE (
-    IF "%PROCESSOR_ARCHITECTURE%"=="x86" IF "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
-        REM Running under 32-bit CMD.EXE on 64-bit Windows...
-        SET builtin_python_path="python-3.6.0-embed-amd64"
+    IF "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
+        SET "builtin_python_path=python-3.6.0-embed-amd64"
     ) ELSE (
-        REM Running under 32-bit CMD.EXE on 32-bit Windows...
-        SET builtin_python_path="python-3.6.0-embed-win32"
+        SET "builtin_python_path=python-3.6.0-embed-win32"
     )
 )
 
+SET "mypath=%~dp0"
 
+IF EXIST "%mypath%%builtin_python_path%\python.exe" (
+    "%mypath%%builtin_python_path%\python.exe" "%mypath%start.py"
+) ELSE (
+    py -3.6 -V >nul 2>&1
 
-
-
-SET mypath=%~dp0
-
-
-
-
-
-
-
-
-IF Exist "%mypath%\%builtin_python_path%\python.exe" (
-  "%mypath%\%builtin_python_path%\python.exe" "%mypath%\start.py"
-) else (
-  
-  
-  
-	py -3.6 -V
-
-	if %errorlevel% equ 0 (
-	  py -3.6 "%mypath%\start.py"
-	) else (
-	  py -3 "%mypath%\start.py"
-	)  
-  
-  
-  
-  
+    IF ERRORLEVEL 0 (
+        py -3.6 "%mypath%start.py"
+    ) ELSE (
+        py -3 "%mypath%start.py"
+    )
 )
 
-
-
-
-
-
-
-
-
-
-
 echo Press any key to exit...
-
-
-
 pause >nul
+
+ENDLOCAL
