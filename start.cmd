78c9504fc6356f72cd70b607b99d8a109fd5cb4e
ocvcoin <90601304+ocvcoin@users.noreply.github.com>
2026-07-12 21:11:44 +0000

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -18,9 +18,11 @@ IF EXIST "%mypath%%builtin_python_path%\python.exe" (
 ) ELSE (
     py -3.6 -V >nul 2>&1
 
-    IF ERRORLEVEL 0 (
+    IF %errorlevel% equ 0 (
+        echo "Found Python 3.6"
         py -3.6 "%mypath%start.py"
     ) ELSE (
+        echo "Found Python 3+"
         py -3 "%mypath%start.py"
     )
 )
